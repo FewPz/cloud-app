64b6c11069e9e50f5d641e9bcc9afa7845454407
phatsanphonna <phatsanphon2004@outlook.com>
2025-10-19 23:42:54 +0700

    Init project

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import { Elysia } from "elysia";
+import { node } from "@elysiajs/node";
+
+const app = new Elysia({ adapter: node() })
+  .ws("/ws", {
+    message(ws, message) {
+      ws.send(message);
+    },
+  })
+  .get("/", () => "Hello Elysia")
+  .listen(3000, ({ hostname, port }) => {
+    console.log(`🦊 Elysia is running at ${hostname}:${port}`);
+  });
